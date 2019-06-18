8f49dd203ffb0ada68c0603ef6c6a72d521fb93c
Brian <riceb53@gmail.com>
2019-06-17 20:53:04 -0500

    model employee using hash with keys as symbols

--- employee.rb
+++ employee.rb
@@ -23,5 +23,15 @@ employee1 = {
 
 p "#{employee1['first_name']} #{employee1['last_name']} makes $#{employee1['salary']} per year."
 
+employee2 = {
+  :first_name => "Danilo",
+  :last_name => "Campos",
+  :salary => 100000,
+  :active => false
+}
+
+
+p "#{employee2[:first_name]} #{employee2[:last_name]} makes $#{employee2[:salary]} per year."
+
 
 # class
