2c8cbb1925cda3fbdf647fb4a94d4d6b4812af26
Brian <riceb53@gmail.com>
2019-06-18 20:33:37 -0500

    DRY up manager class

--- manager.rb
+++ manager.rb
@@ -25,25 +25,7 @@ employee2.print_info
 # employee2.send_report
 
 
-class Manager
-  attr_reader :first_name, :last_name
-  attr_writer :first_name
-
-  def initialize(input_options)
-    @first_name = input_options[:first_name]
-    @last_name = input_options[:last_name]
-    @salary = input_options[:salary]
-    @active = input_options[:active]
-  end
-
-  def print_info
-    p "#{@first_name} #{@last_name} makes $#{@salary} per year."
-  end
-
-  def give_annual_raise
-    @salary *= 1.05
-  end
-
+class Manager < Employee
   def send_report
     p "going to send that report..."
     # some sort of logic in here
